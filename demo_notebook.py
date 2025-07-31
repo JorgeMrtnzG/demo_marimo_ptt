1015da4caaf48843ac708dcca023340f0ea88b04
Jorge Martinez <jorgemartinez@Jorges-MacBook-Air.local>
2025-07-31 14:36:53 -0500

    Update notebook

--- demo_notebook.py
+++ demo_notebook.py
@@ -21,23 +21,56 @@ def _():
 
 
 @app.cell
-def _(TILES, folium, gpd, mo):
-    url = f"{mo.notebook_location()}/public/adm2_risaralda.geojson"
+def _(gpd, mo):
+    _url = f"{mo.notebook_location()}/public/adm2_risaralda.geojson"
+    boundaries_gdf = gpd.read_file(_url)
 
-    gdf = gpd.read_file(url)
-    _centroid = gdf.dissolve().centroid.to_dict()[0]
-
-    map = folium.Map(tiles=TILES, min_zoom=5, location=(_centroid.y, _centroid.x))
-    folium.GeoJson(gdf, layer_name="boundaries").add_to(map)
-
-
-    map
-
-    return
+    _url = f"{mo.notebook_location()}/public/amenities.geojson"
+    amenities_gdf = gpd.read_file(_url)
+    return amenities_gdf, boundaries_gdf
 
 
 @app.cell
-def _():
+def _(amenities_gdf, boundaries_gdf, mo):
+    _amenities_options = amenities_gdf.amenity.unique().tolist()
+    amenities_dropdown = mo.ui.dropdown(label="Amenity", options=_amenities_options, searchable=True)
+    amenities_dropdown
+
+    _bnd_options = boundaries_gdf.gaul2_name.unique().tolist()
+    bnd_dropdown = mo.ui.dropdown(label="Municipality", options=_bnd_options, searchable=True)
+
+    mo.hstack([bnd_dropdown, amenities_dropdown], justify="start")
+    return amenities_dropdown, bnd_dropdown
+
+
+@app.cell
+def _(
+    TILES,
+    amenities_dropdown,
+    amenities_gdf,
+    bnd_dropdown,
+    boundaries_gdf,
+    folium,
+    gpd,
+):
+    _filtered_bnd_gdf = boundaries_gdf if bnd_dropdown.value is None else boundaries_gdf.loc[boundaries_gdf.gaul2_name == bnd_dropdown.value]
+
+    _filtered_gdf = gpd.overlay(amenities_gdf, _filtered_bnd_gdf, how="intersection")[["name", "amenity", "geometry"]]
+    _filtered_gdf = _filtered_gdf if amenities_dropdown.value is None else _filtered_gdf.loc[_filtered_gdf.amenity == amenities_dropdown.value]
+
+    _centroid = _filtered_bnd_gdf.dissolve().centroid.to_dict()[0]
+
+    map = folium.Map(tiles=TILES, location=(_centroid.y, _centroid.x), min_zoom=11)
+
+    _tooltip = folium.GeoJsonTooltip(fields=["name"])
+
+    folium.GeoJson(_filtered_bnd_gdf).add_to(map)
+
+    if _filtered_gdf.shape[0] > 0:
+        folium.GeoJson(_filtered_gdf, layer_name="amenities", marker=folium.Circle(weigth=10, radius=50, stroke=False, fill=True, fill_opacity=0.6, opacity=1), tooltip=_tooltip).add_to(map)
+
+    map
+
     return
 
 
