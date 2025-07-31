a7ce8f2535defe7bcfed812c48980523ad0654d9
Jorge Martinez <jorgemartinez@Jorges-MacBook-Air.local>
2025-07-31 15:05:29 -0500

    Fix file download

--- demo_notebook.py
+++ demo_notebook.py
@@ -15,31 +15,46 @@ def _():
     import marimo as mo
     import geopandas as gpd
     import folium
+    import requests
+
+    from os.path import join
 
     TILES = "Cartodb dark_matter"
-    return TILES, folium, gpd, mo
+    return TILES, folium, gpd, join, mo, requests
 
 
 @app.cell
-def _(gpd, mo):
+def _(gpd, join, mo, requests, shutil):
+    def get_file(location: str, file_name: str) -> str:
+        file_path = join(location, "public", file_name)
+        if location.startswith("http") is False:
+            return file_path
+
+        local_file = join(mo.notebook_dir(), file_name)
+        with requests.get(file_path, stream=True) as resp:
+            with open(local_file, "wb") as f:
+                shutil.copyfileobj(resp.raw, f)
+
+        return local_file
+
     _location = mo.notebook_location().as_posix()
-
-    _url = f"{_location}/public/adm2_risaralda.geojson"
-    boundaries_gdf = gpd.read_file(_url)
-
-    _url = f"{_location}/public/amenities.geojson"
-    amenities_gdf = gpd.read_file(_url)
+    boundaries_gdf = gpd.read_file(get_file(_location, "adm2_risaralda.geojson"))
+    amenities_gdf = gpd.read_file(get_file(_location, "amenities.geojson"))
     return amenities_gdf, boundaries_gdf
 
 
 @app.cell
 def _(amenities_gdf, boundaries_gdf, mo):
     _amenities_options = amenities_gdf.amenity.unique().tolist()
-    amenities_dropdown = mo.ui.dropdown(label="Amenity", options=_amenities_options, searchable=True)
+    amenities_dropdown = mo.ui.dropdown(
+        label="Amenity", options=_amenities_options, searchable=True
+    )
     amenities_dropdown
 
     _bnd_options = boundaries_gdf.gaul2_name.unique().tolist()
-    bnd_dropdown = mo.ui.dropdown(label="Municipality", options=_bnd_options, searchable=True)
+    bnd_dropdown = mo.ui.dropdown(
+        label="Municipality", options=_bnd_options, searchable=True
+    )
 
     mo.hstack([bnd_dropdown, amenities_dropdown], justify="start")
     return amenities_dropdown, bnd_dropdown
@@ -55,10 +70,20 @@ def _(
     folium,
     gpd,
 ):
-    _filtered_bnd_gdf = boundaries_gdf if bnd_dropdown.value is None else boundaries_gdf.loc[boundaries_gdf.gaul2_name == bnd_dropdown.value]
+    _filtered_bnd_gdf = (
+        boundaries_gdf
+        if bnd_dropdown.value is None
+        else boundaries_gdf.loc[boundaries_gdf.gaul2_name == bnd_dropdown.value]
+    )
 
-    _filtered_gdf = gpd.overlay(amenities_gdf, _filtered_bnd_gdf, how="intersection")[["name", "amenity", "geometry"]]
-    _filtered_gdf = _filtered_gdf if amenities_dropdown.value is None else _filtered_gdf.loc[_filtered_gdf.amenity == amenities_dropdown.value]
+    _filtered_gdf = gpd.overlay(amenities_gdf, _filtered_bnd_gdf, how="intersection")[
+        ["name", "amenity", "geometry"]
+    ]
+    _filtered_gdf = (
+        _filtered_gdf
+        if amenities_dropdown.value is None
+        else _filtered_gdf.loc[_filtered_gdf.amenity == amenities_dropdown.value]
+    )
 
     _centroid = _filtered_bnd_gdf.dissolve().centroid.to_dict()[0]
 
@@ -69,7 +94,19 @@ def _(
     folium.GeoJson(_filtered_bnd_gdf).add_to(map)
 
     if _filtered_gdf.shape[0] > 0:
-        folium.GeoJson(_filtered_gdf, layer_name="amenities", marker=folium.Circle(weigth=10, radius=50, stroke=False, fill=True, fill_opacity=0.6, opacity=1), tooltip=_tooltip).add_to(map)
+        folium.GeoJson(
+            _filtered_gdf,
+            layer_name="amenities",
+            marker=folium.Circle(
+                weigth=10,
+                radius=50,
+                stroke=False,
+                fill=True,
+                fill_opacity=0.6,
+                opacity=1,
+            ),
+            tooltip=_tooltip,
+        ).add_to(map)
 
     map
 
